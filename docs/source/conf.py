0f18e18f9ac753c13adfd8baf52413b5f68fbea6
Tino Bischoff <tino.bischoff@iosb.fraunhofer.de>
2024-09-25 11:23:39 +0200

    update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,8 +14,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
-    'myst_parser',
-    'sphinx_markdown_tables'
+    'myst_parser'
 ]
 
 intersphinx_mapping = {
