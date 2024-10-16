2d4fedb674811bfc6e7a7b2c7679604cbe738e54
Tino Bischoff <tino.bischoff@iosb.fraunhofer.de>
2024-10-16 17:52:59 +0200

    add description integration test

---
+++ service/src/test/java/de/fraunhofer/iosb/ilt/faaast/registry/service/DescriptionControllerIT.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2021 Fraunhofer IOSB, eine rechtlich nicht selbstaendige
+ * Einrichtung der Fraunhofer-Gesellschaft zur Foerderung der angewandten
+ * Forschung e.V.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.fraunhofer.iosb.ilt.faaast.registry.service;
+
+import de.fraunhofer.iosb.ilt.faaast.service.model.ServiceDescription;
+import de.fraunhofer.iosb.ilt.faaast.service.model.ServiceSpecificationProfile;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = App.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(locations = "classpath:application-integrationtest.properties")
+public class DescriptionControllerIT {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void testDescription() {
+        ResponseEntity<ServiceDescription> response = restTemplate.exchange(createURLWithPort(""), HttpMethod.GET, null, ServiceDescription.class);
+        Assert.assertNotNull(response);
+        Assert.assertEquals(HttpStatus.OK, response.getStatusCode());
+        Assert.assertNotNull(response.getBody());
+        ServiceDescription expected = ServiceDescription.builder()
+                .profile(ServiceSpecificationProfile.AAS_REGISTRY_FULL)
+                .profile(ServiceSpecificationProfile.SUBMODEL_REGISTRY_FULL)
+                .build();
+        Assert.assertEquals(expected, response.getBody());
+    }
+
+
+    private String createURLWithPort(String uri) {
+        return "http://localhost:" + port + "/api/v3.0/description" + uri;
+    }
+}
