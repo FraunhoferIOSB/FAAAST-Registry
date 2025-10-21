1cb31f969131bcd4fd32be0676bdba5ac2e51590
Tino Bischoff <tino.bischoff@iosb.fraunhofer.de>
2025-10-21 17:55:09 +0200

    integrate support for at+jwt token type

--- service/src/main/java/de/fraunhofer/iosb/ilt/faaast/registry/service/SecurityConfig.java
+++ service/src/main/java/de/fraunhofer/iosb/ilt/faaast/registry/service/SecurityConfig.java
@@ -14,7 +14,18 @@
  */
 package de.fraunhofer.iosb.ilt.faaast.registry.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nimbusds.jose.JOSEObjectType;
+import com.nimbusds.jose.proc.DefaultJOSEObjectTypeVerifier;
 import de.fraunhofer.iosb.ilt.faaast.registry.service.helper.AclFilter;
+import de.fraunhofer.iosb.ilt.faaast.service.util.EncodingHelper;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.HashMap;
+import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,6 +34,10 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtException;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
@@ -34,8 +49,15 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private static final String JWT_TYP = "typ";
+    private static final String JWT_AT_JWT = "at+jwt";
+    private static final Logger LOGGER = LoggerFactory.getLogger(SecurityConfig.class);
+
     @Value("${service.security.aclFolder}")
     private String aclFolder;
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    private String issuerUri;
 
     /**
      * Configure access.
@@ -61,4 +83,66 @@ public class SecurityConfig {
         return http.build();
     }
 
+
+    /**
+     * A JWT Decoder.
+     *
+     * @return The JWT Decoder.
+     */
+    @Bean
+    public JwtDecoder jwtDecoder() {
+
+        JwtDecoder jwtDecoderDefault = NimbusJwtDecoder.withIssuerLocation(issuerUri).build();
+        JwtDecoder jwtDecoderAt = NimbusJwtDecoder.withIssuerLocation(issuerUri).jwtProcessorCustomizer(customizer -> {
+            customizer.setJWSTypeVerifier(new DefaultJOSEObjectTypeVerifier<>(new JOSEObjectType("at+jwt")));
+        }).build();
+
+        return new JwtDecoder() {
+            @Override
+            public Jwt decode(String token) throws JwtException {
+                try {
+                    LOGGER.info("token: {}", token);
+                    String[] chunks = token.split("\\.");
+                    String header = EncodingHelper.base64UrlDecode(chunks[0]);
+                    LOGGER.info("Header: {}", header);
+                    //JwtParser jwtParser = Jwts.parser().build();
+                    //jwtParser.parse(chunks{0]).
+                    Map<String, Object> headerMapping = new ObjectMapper().readValue(header, HashMap.class);
+                    Jwt jwt;
+                    if (headerMapping.containsKey(JWT_TYP)) {
+                        String typ = headerMapping.get(JWT_TYP).toString();
+                        if (JWT_AT_JWT.equals(typ)) {
+                            LOGGER.debug("jwtDecoder: use at+jwt Decoder (Typ: {})", typ);
+                            jwt = jwtDecoderAt.decode(token);
+                        }
+                        else {
+                            LOGGER.debug("jwtDecoder: use default Decoder (Typ: {})", typ);
+                            jwt = jwtDecoderDefault.decode(token);
+                        }
+                    }
+                    else {
+                        LOGGER.debug("jwtDecoder: use default Decoder (unknown Typ");
+                        jwt = jwtDecoderDefault.decode(token);
+                    }
+
+                    //LOGGER.info("jwt: {}", jwt);
+                    LOGGER.debug("jwt ID: {}", jwt.getId());
+                    LOGGER.debug("jwt Subject: {}", jwt.getSubject());
+                    LOGGER.debug("jwt token expires: {}", LocalDateTime.ofInstant(jwt.getExpiresAt(), ZoneId.systemDefault()));
+                    //for (var c: jwt.getClaims().entrySet()) {
+                    //    LOGGER.info("Claim: Key: {}; Value: {}", c.getKey(), c.getValue());
+                    //}
+                    return jwt;
+                }
+                catch (JwtException ex) {
+                    LOGGER.error("exception: ", ex);
+                    throw ex;
+                }
+                catch (JsonProcessingException ex) {
+                    LOGGER.error("exception: ", ex);
+                    throw new JwtException(ex.getMessage());
+                }
+            }
+        };
+    }
 }
