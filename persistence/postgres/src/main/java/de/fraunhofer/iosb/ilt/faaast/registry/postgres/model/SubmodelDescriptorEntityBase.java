a4cb26113d9d898dfed305add50fb658520b79d8
Tino Bischoff <tino.bischoff@iosb.fraunhofer.de>
2026-05-06 17:52:59 +0200

    add SubmodelDescriptor attributes

--- persistence/postgres/src/main/java/de/fraunhofer/iosb/ilt/faaast/registry/postgres/model/SubmodelDescriptorEntityBase.java
+++ persistence/postgres/src/main/java/de/fraunhofer/iosb/ilt/faaast/registry/postgres/model/SubmodelDescriptorEntityBase.java
@@ -15,6 +15,8 @@
 package de.fraunhofer.iosb.ilt.faaast.registry.postgres.model;
 
 import org.eclipse.digitaltwin.aas4j.v3.model.builder.ExtendableBuilder;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
 
 /**
@@ -23,6 +25,27 @@ import org.eclipse.digitaltwin.aas4j.v3.model.builder.ExtendableBuilder;
 public class SubmodelDescriptorEntityBase {
 
     private String idShort;
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    private String administration;
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    private String endpoints;
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    private String description;
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    private String displayName;
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    private String extensions;
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    private String semanticId;
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    private String supplementalSemanticId;
 
     public String getIdShort() {
         return idShort;
@@ -33,10 +56,136 @@ public class SubmodelDescriptorEntityBase {
         this.idShort = idShort;
     }
 
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    public String getAdministration() {
+        return administration;
+    }
+
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    public void setAdministration(String administration) {
+        this.administration = administration;
+    }
+
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    public String getEndpoints() {
+        return endpoints;
+    }
+
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    public void setEndpoints(String endpoints) {
+        this.endpoints = endpoints;
+    }
+
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    public String getDescription() {
+        return description;
+    }
+
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    public String getDisplayName() {
+        return displayName;
+    }
+
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    public String getExtensions() {
+        return extensions;
+    }
+
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    public void setExtensions(String extensions) {
+        this.extensions = extensions;
+    }
+
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    public String getSupplementalSemanticId() {
+        return supplementalSemanticId;
+    }
+
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    public void setSemanticId(String semanticId) {
+        this.semanticId = semanticId;
+    }
+
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    public String getSemanticId() {
+        return semanticId;
+    }
+
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    public void setSupplementalSemanticId(String supplementalSemanticId) {
+        this.supplementalSemanticId = supplementalSemanticId;
+    }
+
     public abstract static class AbstractBuilder<T extends SubmodelDescriptorEntityBase, B extends AbstractBuilder<T, B>> extends ExtendableBuilder<T, B> {
 
         public B idShort(String value) {
             getBuildingInstance().setIdShort(value);
+            return getSelf();
+        }
+
+
+        public B administration(String value) {
+            getBuildingInstance().setAdministration(value);
+            return getSelf();
+        }
+
+
+        public B endpoints(String value) {
+            getBuildingInstance().setEndpoints(value);
+            return getSelf();
+        }
+
+
+        public B description(String value) {
+            getBuildingInstance().setDescription(value);
+            return getSelf();
+        }
+
+
+        public B displayName(String value) {
+            getBuildingInstance().setDisplayName(value);
+            return getSelf();
+        }
+
+
+        public B extensions(String value) {
+            getBuildingInstance().setExtensions(value);
+            return getSelf();
+        }
+
+
+        public B semanticId(String value) {
+            getBuildingInstance().setSemanticId(value);
+            return getSelf();
+        }
+
+
+        public B supplementalSemanticId(String value) {
+            getBuildingInstance().setSupplementalSemanticId(value);
             return getSelf();
         }
     }
--- persistence/postgres/src/main/java/de/fraunhofer/iosb/ilt/faaast/registry/postgres/util/ModelTransformationHelper.java
+++ persistence/postgres/src/main/java/de/fraunhofer/iosb/ilt/faaast/registry/postgres/util/ModelTransformationHelper.java
@@ -17,6 +17,7 @@ package de.fraunhofer.iosb.ilt.faaast.registry.postgres.util;
 import de.fraunhofer.iosb.ilt.faaast.registry.postgres.model.AssetAdministrationShellDescriptorEntity;
 import de.fraunhofer.iosb.ilt.faaast.registry.postgres.model.SubmodelDescriptorEntity;
 import de.fraunhofer.iosb.ilt.faaast.service.dataformat.json.DeserializerWrapper;
+import de.fraunhofer.iosb.ilt.faaast.service.util.LambdaExceptionHelper;
 import java.util.List;
 import org.eclipse.digitaltwin.aas4j.v3.dataformat.core.DeserializationException;
 import org.eclipse.digitaltwin.aas4j.v3.dataformat.core.SerializationException;
@@ -27,6 +28,7 @@ import org.eclipse.digitaltwin.aas4j.v3.model.Endpoint;
 import org.eclipse.digitaltwin.aas4j.v3.model.Extension;
 import org.eclipse.digitaltwin.aas4j.v3.model.LangStringNameType;
 import org.eclipse.digitaltwin.aas4j.v3.model.LangStringTextType;
+import org.eclipse.digitaltwin.aas4j.v3.model.Reference;
 import org.eclipse.digitaltwin.aas4j.v3.model.SpecificAssetId;
 import org.eclipse.digitaltwin.aas4j.v3.model.SubmodelDescriptor;
 import org.eclipse.digitaltwin.aas4j.v3.model.impl.DefaultAssetAdministrationShellDescriptor;
@@ -79,7 +81,7 @@ public class ModelTransformationHelper {
      *
      * @param aas The AssetAdministrationShellDescriptorEntity.
      * @return The converted AssetAdministrationShellDescriptor.
-     * @throws DeserializationException If a serialization error occurs.
+     * @throws DeserializationException If a deserialization error occurs.
      */
     public static AssetAdministrationShellDescriptor convertAAS(AssetAdministrationShellDescriptorEntity aas) throws DeserializationException {
         if (aas == null) {
@@ -125,8 +127,9 @@ public class ModelTransformationHelper {
      *
      * @param submodel The desired SubmodelDescriptor.
      * @return The converted SubmodelDescriptorEntity.
+     * @throws SerializationException If a serialization error occurs.
      */
-    public static SubmodelDescriptorEntity convertSubmodel(SubmodelDescriptor submodel) {
+    public static SubmodelDescriptorEntity convertSubmodel(SubmodelDescriptor submodel) throws SerializationException {
         if (submodel == null) {
             return null;
         }
@@ -134,6 +137,13 @@ public class ModelTransformationHelper {
         return new SubmodelDescriptorEntity.Builder()
                 .id(submodel.getId())
                 .idShort(submodel.getIdShort())
+                .administration(jsonSerializer.write(submodel.getAdministration()))
+                .endpoints(jsonSerializer.write(submodel.getEndpoints()))
+                .description(jsonSerializer.write(submodel.getDescription()))
+                .displayName(jsonSerializer.write(submodel.getDisplayName()))
+                .extensions(jsonSerializer.write(submodel.getExtensions()))
+                .semanticId(jsonSerializer.write(submodel.getSemanticId()))
+                .supplementalSemanticId(jsonSerializer.write(submodel.getSupplementalSemanticId()))
                 .build();
     }
 
@@ -143,16 +153,39 @@ public class ModelTransformationHelper {
      *
      * @param submodel The desired SubmodelDescriptorEntity.
      * @return The converted SubmodelDescriptorEntity.
+     * @throws DeserializationException If a serialization error occurs.
      */
-    public static SubmodelDescriptor convertSubmodel(SubmodelDescriptorEntity submodel) {
+    public static SubmodelDescriptor convertSubmodel(SubmodelDescriptorEntity submodel) throws DeserializationException {
         if (submodel == null) {
             return null;
         }
 
-        return new DefaultSubmodelDescriptor.Builder()
+        DefaultSubmodelDescriptor.Builder builder = new DefaultSubmodelDescriptor.Builder()
                 .id(submodel.getId())
-                .idShort(submodel.getIdShort())
-                .build();
+                .idShort(submodel.getIdShort());
+
+        if ((submodel.getAdministration() != null) && (!submodel.getAdministration().isEmpty())) {
+            builder.administration(jsonDeserializer.read(submodel.getAdministration(), AdministrativeInformation.class));
+        }
+        if ((submodel.getEndpoints() != null) && (!submodel.getEndpoints().isEmpty())) {
+            builder.endpoints(jsonDeserializer.readList(submodel.getEndpoints(), Endpoint.class));
+        }
+        if ((submodel.getDescription() != null) && (!submodel.getDescription().isEmpty())) {
+            builder.description(jsonDeserializer.readList(submodel.getDescription(), LangStringTextType.class));
+        }
+        if ((submodel.getDisplayName() != null) && (!submodel.getDisplayName().isEmpty())) {
+            builder.displayName(jsonDeserializer.readList(submodel.getDisplayName(), LangStringNameType.class));
+        }
+        if ((submodel.getExtensions() != null) && (!submodel.getExtensions().isEmpty())) {
+            builder.extensions(jsonDeserializer.readList(submodel.getExtensions(), Extension.class));
+        }
+        if ((submodel.getSemanticId() != null) && (!submodel.getSemanticId().isEmpty())) {
+            builder.semanticId(jsonDeserializer.read(submodel.getSemanticId(), Reference.class));
+        }
+        if ((submodel.getSupplementalSemanticId() != null) && (!submodel.getSupplementalSemanticId().isEmpty())) {
+            builder.supplementalSemanticId(jsonDeserializer.readList(submodel.getSupplementalSemanticId(), Reference.class));
+        }
+        return builder.build();
     }
 
 
@@ -161,14 +194,15 @@ public class ModelTransformationHelper {
      *
      * @param submodels The desired list of SubmodelDescriptor.
      * @return The converted list of SubmodelDescriptorEntity.
+     * @throws SerializationException If a serialization error occurs.
      */
-    public static List<SubmodelDescriptorEntity> convertSubmodels(List<SubmodelDescriptor> submodels) {
+    public static List<SubmodelDescriptorEntity> convertSubmodels(List<SubmodelDescriptor> submodels) throws SerializationException {
         if (submodels == null) {
             return null;
         }
 
         return submodels.stream()
-                .map(s -> convertSubmodel(s))
+                .map(LambdaExceptionHelper.rethrowFunction(s -> convertSubmodel(s)))
                 .toList();
     }
 
@@ -178,14 +212,15 @@ public class ModelTransformationHelper {
      *
      * @param submodels The desired list of SubmodelDescriptorEntities.
      * @return The converted list of SubmodelDescriptors.
+     * @throws DeserializationException If a deserialization error occurs.
      */
-    public static List<SubmodelDescriptor> convertSubmodelsEntity(List<SubmodelDescriptorEntity> submodels) {
+    public static List<SubmodelDescriptor> convertSubmodelsEntity(List<SubmodelDescriptorEntity> submodels) throws DeserializationException {
         if (submodels == null) {
             return null;
         }
 
         return submodels.stream()
-                .map(s -> convertSubmodel(s))
+                .map(LambdaExceptionHelper.rethrowFunction(s -> convertSubmodel(s)))
                 .toList();
     }
 }
